d81a84bc7cf090093b856bb94c7e63634468e5ab
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-10-13 23:54:34 +0530

    Febonacci Series

--- src/LogicalProgram/FebonacciSeries.java
+++ src/LogicalProgram/FebonacciSeries.java
@@ -1,2 +1,22 @@
-package LogicalProgram;public class FebonacciSeries {
+package LogicalProgram;
+
+import java.util.Scanner;
+
+public class FebonacciSeries {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int a =0; int b =1;
+        System.out.println(a + " " + b);
+        int c;
+        for (int i=1;i<n; i++ ){
+            c= a+b;
+            System.out.println(" " +c);
+            a=b;
+            b=c;
+        }
+
+    }
 }
+
+
